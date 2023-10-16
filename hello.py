33514c924f43d5a3fd2979e78d1e1b36a50e4417
Nazmul Alam <shawonkst15@gmail.com>
2023-10-16 17:31:59 +0600

    add new file

---
+++ hello.py
@@ -0,0 +1,11 @@
+#  List : ordered,mutable,allow dupicate
+myList = ["banana", "Cherry", "apple"]
+print(myList)
+item = myList[-1]
+print(item)
+for i in myList:
+  print(i)
+if "mango" in myList:
+    print ("Yes")
+else:
+    print("no")
