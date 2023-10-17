9cb4c2b9f91be3303ba7d8a8898e8f7612d65d66
Nazmul Alam <shawonkst15@gmail.com>
2023-10-17 18:20:22 +0600

    add file

--- dictonary.py
+++ dictonary.py
@@ -1,1 +1,8 @@
-mydict = {"name":"Nazmul","age":34,}
+mydict = {"name": "Nazmul", "age": 34, "city": "Kushtia", }
+print(mydict)
+mydic2 = dict(name="Alam", age=34, city="Kushtia")
+print(mydict)
+value = mydict["city"]
+print(value)
+mydict["emai"] ="shawonkst17@gmail.com"
+print(mydict)
